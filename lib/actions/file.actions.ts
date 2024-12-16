077d0e6fa0258db76a063ae28eff96c0364e25b3
Juan Espinosa <arq.jorrin@gmail.com>
2024-12-16 22:56:23 +0800

    Filter files and global search

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -52,7 +52,7 @@ export const uploadFile = async ({file, ownerId, accountId, path}: UploadFilePro
   };
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
   const queries = [
     Query.or([
       Query.equal('owner', [currentUser.$id]),
@@ -60,12 +60,18 @@ const createQueries = (currentUser: Models.Document) => {
     ])
   ];
 
-  // TODO: search, sort, limits ...
+  if (types.length > 0) queries.push(Query.equal('type', types));
+  if (searchText) queries.push(Query.contains('name', searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  const [sortBy, orderBy] = sort.split('-');
+
+  queries.push(orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
 
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [], searchText = '', sort = '$createdAt-desc', limit }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -73,7 +79,7 @@ export const getFiles = async () => {
 
     if(!currentUser) throw new Error('User not found');
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
